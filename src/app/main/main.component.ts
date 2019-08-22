d68f0aa0a9ad4ea24bbd4ae292efa25db89e2785
damien verkerk <damien-verkerk@hotmail.com>
2019-08-22 16:16:13 +0200

    mock-text toegevoegd en aanpassingen gemaakt in main.component zodat tekst getoond wordt

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+import { TextService } from '../text.service';
+import { savedText } from '../text';
+
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
-
-  constructor() { }
+  text: savedText[];
+  constructor( private textService : TextService) { }
+  
+  getText():void{
+    this.text = this.textService.getText();
+  }
 
   ngOnInit() {
+    this.getText();
   }
 
 }
--- src/app/text.service.ts
+++ src/app/text.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
 
+import { TEXT } from './mock-text';
+import { savedText } from './text';
+
 @Injectable({
   providedIn: 'root'
 })
 export class TextService {
-
-  constructor() { }
+  getText(): savedText[]{
+    return TEXT;
+  }
 }
