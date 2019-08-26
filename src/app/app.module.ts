870b42966797371ca29025b986345c39627c52de
damien verkerk <damien-verkerk@hotmail.com>
2019-08-26 20:56:52 +0200

    Zoekbalk toegevoegd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
 import { SavedTextComponent } from './saved-text/saved-text.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
-    SavedTextComponent
+    SavedTextComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
